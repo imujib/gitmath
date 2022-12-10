1d5153a7a30080f688a26761c8cbca7ceb945361
imujib <imamssoft@gmail.com>
2022-12-10 08:54:30 +0700

    add add_f() function

--- src/main.c
+++ src/main.c
@@ -5,9 +5,11 @@
 int main(int argc, char **argv)
 {
 	int a=15, b=3;
+	double p=20.75, q=5.25;
 	
 	puts("*** mathlib demo ***");
 	printf("add_i(%d,%d)=%d\n",a,b,add_i(a,b));
+	printf("add_f(%.3lf,%.3lf)=%.3lf\n",p,q,add_f(p,q));
 
 	return 0;
 }
