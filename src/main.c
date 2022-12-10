d1852c0ba6932efd2fbfccda4e5066e7d2248759
imujib <imamssoft@gmail.com>
2022-12-10 09:02:19 +0700

    add mul_f() function

--- src/main.c
+++ src/main.c
@@ -5,9 +5,11 @@
 int main(int argc, char **argv)
 {
 	int a=15, b=3;
+	double p=20.75, q=5.25;
 
 	puts("*** mathlib demo ***");
 	printf("mul_i(%d,%d)=%d\n",a,b,mul_i(a,b));
+	printf("mul_f(%.3lf,%.3lf)=%.3lf\n",p,q,mul_f(p,q));
 
 	return 0;
 }
